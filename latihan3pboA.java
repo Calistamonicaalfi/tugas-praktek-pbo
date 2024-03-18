bb6e37ab717dd24735e72e2f34e63f6adf4480a5
Calistamonicaalfi <161101838+Calistamonicaalfi@users.noreply.github.com>
2024-03-18 13:00:28 +0700

    Add files via upload

---
+++ latihan3pboA.java
@@ -0,0 +1,14 @@
+/*
+
+nama : calista monica alfi
+no nim : 2301093013
+soal no 3 for loop
+*/
+public class latihan3pboA {
+    public static void main(String[] args) {
+        for ( int i = 0; i <= 100;i++)
+        {
+            System.out.println("calista monica alfi");
+        }
+    }
+}
---
+++ latihan3pboB.java
@@ -0,0 +1,15 @@
+/*
+
+nama : calista monica alfi
+no nim : 2301093013
+soal 3 do while
+*/
+public class latihan3pboB {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("calista monica alfi");
+            i++;
+        } while (i <= 100);
+    }
+}
